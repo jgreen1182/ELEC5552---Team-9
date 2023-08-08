11fe79613b41690cc7e6d9607f7d9d3a56d01407
jgreen1182 <103915420+jgreen1182@users.noreply.github.com>
2023-08-08 17:30:03 +0800

    Update Encoder_Test.ino

--- Microcontroller/Software/Draft/Encoder_Test.ino
+++ Microcontroller/Software/Draft/Encoder_Test.ino
@@ -1,4 +1,3 @@
-#based on https://lastminuteengineers.com/rotary-encoder-arduino-tutorial/
 #include <LiquidCrystal.h>
 
 // Initialize the LCD
@@ -7,18 +6,22 @@ LiquidCrystal lcd(12, 11, 5, 4, 3, 2); // Adjust these pin numbers to match your
 // Rotary Encoder Inputs
 #define CLK 8
 #define DT 9
+#define SW 10
 #define POT A0
 
 int counter = 0;
 int currentStateCLK;
 int lastStateCLK;
 String currentDir ="";
+bool fineAdjust = false;
+int lastButtonState = HIGH;
 
 void setup() {
         
   // Set encoder pins as inputs
   pinMode(CLK,INPUT);
   pinMode(DT,INPUT);
+  pinMode(SW,INPUT_PULLUP);
 
   // Setup Serial Monitor
   Serial.begin(9600);
@@ -31,15 +34,22 @@ void setup() {
 
   // Print the static text on the LCD
   lcd.setCursor(0, 0);
-  lcd.print("Pot Duty: ");
+  lcd.print("Pot Duty:");
   lcd.setCursor(0, 1);
-  lcd.print("Enc Duty: ");
+  lcd.print("Enc Duty:");
 }
 
 void loop() {
         
   // Read the current state of CLK
   currentStateCLK = digitalRead(CLK);
+
+  // Check if the switch is pressed
+  if (digitalRead(SW) == LOW && lastButtonState == HIGH) {
+    fineAdjust = !fineAdjust;
+    delay(200); // Debounce delay
+  }
+  lastButtonState = digitalRead(SW);
 
   // If last and current state of CLK are different, then pulse occurred
   // React to only 1 state change to avoid double count
@@ -48,16 +58,16 @@ void loop() {
     // If the DT state is different than the CLK state then
     // the encoder is rotating CCW so decrement
     if (digitalRead(DT) != currentStateCLK) {
-      counter ++;
+      counter += fineAdjust ? 1 : 40;
       currentDir ="CW";
     } else {
       // Encoder is rotating CW so increment
-      counter --;
+      counter -= fineAdjust ? 1 : 40;
       currentDir ="CCW";
     }
 
-    // Constrain the counter value between 0 and 1023
-    counter = constrain(counter, 0, 1023);
+    // Constrain the counter value between 0 and 4000
+    counter = constrain(counter, 0, 4000);
   }
 
   // Read the value from the potentiometer
@@ -65,19 +75,19 @@ void loop() {
 
   // Calculate the duty cycles as percentages
   float potDutyCyclePercentage = (float)potValue / 1023.0 * 100.0;
-  float encoderDutyCyclePercentage = (float)counter / 1023.0 * 100.0;
+  float encoderDutyCyclePercentage = (float)counter / 4000.0 * 100.0;
 
   // Display the duty cycles on the LCD
-  lcd.setCursor(10, 0);
+  lcd.setCursor(9, 0);
   lcd.print("     "); // Clear the previous value
-  lcd.setCursor(10, 0);
-  lcd.print(potDutyCyclePercentage);
+  lcd.setCursor(9, 0);
+  lcd.print(potDutyCyclePercentage, 3);
   lcd.print("%");
 
-  lcd.setCursor(10, 1);
+  lcd.setCursor(9, 1);
   lcd.print("     "); // Clear the previous value
-  lcd.setCursor(10, 1);
-  lcd.print(encoderDutyCyclePercentage);
+  lcd.setCursor(9, 1);
+  lcd.print(encoderDutyCyclePercentage, 3);
   lcd.print("%");
 
   // Remember last CLK state
