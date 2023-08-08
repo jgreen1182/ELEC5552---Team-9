93dfb4bcbffec3f3976979a3f9a8e2eefc6e8cc0
jgreen1182 <103915420+jgreen1182@users.noreply.github.com>
2023-08-08 17:13:43 +0800

    Update Encoder_Test.ino

--- Microcontroller/Software/Draft/Encoder_Test.ino
+++ Microcontroller/Software/Draft/Encoder_Test.ino
@@ -1,30 +1,88 @@
-const int encoderPin1 = 8; // CLK pin for the rotary encoder
-const int encoderPin2 = 9; // DT pin for the rotary encoder
+#based on https://lastminuteengineers.com/rotary-encoder-arduino-tutorial/
+#include <LiquidCrystal.h>
 
-int pin1State;
-int lastPin1State = LOW;
+// Initialize the LCD
+LiquidCrystal lcd(12, 11, 5, 4, 3, 2); // Adjust these pin numbers to match your setup
 
-// Define the debounce delay in milliseconds
-const int debounceDelay = 5;
+// Rotary Encoder Inputs
+#define CLK 8
+#define DT 9
+#define POT A0
+
+int counter = 0;
+int currentStateCLK;
+int lastStateCLK;
+String currentDir ="";
 
 void setup() {
-  pinMode(encoderPin1, INPUT);
-  pinMode(encoderPin2, INPUT);
+        
+  // Set encoder pins as inputs
+  pinMode(CLK,INPUT);
+  pinMode(DT,INPUT);
+
+  // Setup Serial Monitor
   Serial.begin(9600);
+
+  // Initialize the LCD with 16 columns and 2 rows
+  lcd.begin(16, 2);
+
+  // Read the initial state of CLK
+  lastStateCLK = digitalRead(CLK);
+
+  // Print the static text on the LCD
+  lcd.setCursor(0, 0);
+  lcd.print("Pot Duty: ");
+  lcd.setCursor(0, 1);
+  lcd.print("Enc Duty: ");
 }
 
 void loop() {
-  pin1State = digitalRead(encoderPin1);
-  if (pin1State != lastPin1State) {
-    delay(debounceDelay); // Add a debounce delay
-    pin1State = digitalRead(encoderPin1); // Read the pin state again after the delay
-    if (pin1State != lastPin1State) { // Check if the pin state has changed
-      if (digitalRead(encoderPin2) != pin1State) {
-        Serial.println("Clockwise");
-      } else {
-        Serial.println("Counter Clockwise");
-      }
+        
+  // Read the current state of CLK
+  currentStateCLK = digitalRead(CLK);
+
+  // If last and current state of CLK are different, then pulse occurred
+  // React to only 1 state change to avoid double count
+  if (currentStateCLK != lastStateCLK  && currentStateCLK == 1){
+
+    // If the DT state is different than the CLK state then
+    // the encoder is rotating CCW so decrement
+    if (digitalRead(DT) != currentStateCLK) {
+      counter ++;
+      currentDir ="CW";
+    } else {
+      // Encoder is rotating CW so increment
+      counter --;
+      currentDir ="CCW";
     }
+
+    // Constrain the counter value between 0 and 1023
+    counter = constrain(counter, 0, 1023);
   }
-  lastPin1State = pin1State;
+
+  // Read the value from the potentiometer
+  int potValue = analogRead(POT);
+
+  // Calculate the duty cycles as percentages
+  float potDutyCyclePercentage = (float)potValue / 1023.0 * 100.0;
+  float encoderDutyCyclePercentage = (float)counter / 1023.0 * 100.0;
+
+  // Display the duty cycles on the LCD
+  lcd.setCursor(10, 0);
+  lcd.print("     "); // Clear the previous value
+  lcd.setCursor(10, 0);
+  lcd.print(potDutyCyclePercentage);
+  lcd.print("%");
+
+  lcd.setCursor(10, 1);
+  lcd.print("     "); // Clear the previous value
+  lcd.setCursor(10, 1);
+  lcd.print(encoderDutyCyclePercentage);
+  lcd.print("%");
+
+  // Remember last CLK state
+  lastStateCLK = currentStateCLK;
+
+  // Put in a slight delay to help debounce the reading
+  delay(1);
 }
